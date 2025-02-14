bbc929b1cfe82bda2efc9c8c2a301fe2afbe2740
Gustavo <gustavoale1759@gmail.com>
2025-02-13 21:44:01 -0300

    Add files via upload
    Signed-off-by: Gustavo  <gustavoale1759@gmail.com>

---
+++ Php básico/2.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+ <title>PHP – Aula Exemplo do Uso de Variáveis</title>
+ </head>
+ <body>
+ <?php
+ $var1 = 1;
+ $varX = 9.50; 
+ $varY = 9.50; 
+$var2 = $varX + $varY; 
+$varSTR = "Atribui String";
+$varletra = "a"; 
+$varbool = true; 
+ echo "Exemplo de uma aplicação em PHP";
+ print "<P>";
+ echo "Conteúdo de var1: ";
+ echo $var1;
+ print "<P>";
+ echo "Conteúdo de varX: ";
+ echo $varX;
+ print "<P>";
+
+ echo "Conteúdo de varY: ";
+ echo $varY;
+ print "<P>";
+ echo "Conteúdo de var2: ";
+ echo $var2;
+ print "<P>";
+
+ echo "Conteúdo de varSTR: ";
+ echo $varSTR;
+ print "<P>";
+
+ echo "Conteúdo de varletra: ";
+ echo $varletra;
+ print "<P>";
+ echo "Conteúdo de varbool: ";
+ echo $varbool;
+ ?>
+ </body>
+ </html>
